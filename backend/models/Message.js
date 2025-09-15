ca8b664484465d39f1c79d4aecca1abf99addbfb
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-09-15 06:27:07 +0530

    Updated the Message.jsx with seen field in it

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -5,6 +5,8 @@ const messageSchema = new mongoose.Schema({
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }, // null for group messages
   group: { type: String, default: null }, // e.g. "global"
   text: { type: String, required: true },
-}, { timestamps: true });
+  seen: { type: Boolean, default: false }, // <-- NEW
+}, 
+{ timestamps: true });
 
 export default mongoose.model("Message", messageSchema);
